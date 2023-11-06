081520acd3368db10c649efb3bdabc0e41f54d03
sampsaoinonen <sampsa.oinonen@gmail.com>
2023-11-06 18:04:57 +0200

    Add more branch coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,45 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_negatiivinen_lisays_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(-5)
+        self.assertEqual(self.varasto.saldo, 0)
+    
+    def test_lisays_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertEqual(self.varasto.saldo, 10)
+
+    def test_ottaminen_yli_saldon(self):
+        self.varasto.lisaa_varastoon(5)
+        otettu = self.varasto.ota_varastosta(10)
+        self.assertEqual(otettu, 5)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_ottaminen_saldon_verran(self):
+        self.varasto.lisaa_varastoon(5)
+        otettu = self.varasto.ota_varastosta(5)
+        self.assertEqual(otettu, 5)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_konstruktori_negatiivisella_tilavuudella(self):
+        varasto_negatiivisella = Varasto(-10)
+        self.assertEqual(varasto_negatiivisella.tilavuus, 0)
+
+    def test_konstruktori_negatiivisella_saldolla(self):
+        varasto_negatiivisella = Varasto(10, -10)
+        self.assertEqual(varasto_negatiivisella.saldo, 0)
+    
+    def test_ota_varastosta_ei_ota_negatiivista_maaraa(self):
+        varasto = Varasto(10, 5)
+        otettu = varasto.ota_varastosta(-3)
+        self.assertEqual(otettu, 0)
+        self.assertEqual(varasto.saldo, 5)
+
+    def test_varasto_str(self):    
+        varasto = Varasto(10, 2)
+        expected_str = "saldo = 2, vielä tilaa 8"    
+        self.assertEqual(str(varasto), expected_str)
+
+
+    
